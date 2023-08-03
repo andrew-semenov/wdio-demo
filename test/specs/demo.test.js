d2e4ef7edafe4ea3b0bfbf76f9441e38fd7ba689
Andrew Semenov <andrew.semenov.sas@gmail.com>
2023-08-03 13:49:57 +0300

    added page object pattern

--- test/specs/demo.test.js
+++ test/specs/demo.test.js
@@ -1,33 +1,20 @@
-import Page from '../pageobjects/page.js'
+// import Page from '../pageobjects/page.js'
+import DeutschPage from '../pageobjects/deutsch.page.js'
+import BestsellerPage from '../pageobjects/bestseller.page.js'
 
 
 describe('Eduki demo', ()=> {
     it('should filter by .pdf format for downloading', async() => {
-        const width = 1920
-        const height = 1080
+        await browser.maximizeWindow()
 
-        browser.setWindowSize(width, height)
+        await DeutschPage.open()
+        await DeutschPage.openBestsellerPage()
 
-        await Page.open('de')
-
-        // Open the Bestseller page
-        const navBestseller = await  $('=Bestseller')
-        await navBestseller.click()
-
-        // Select the Download format
-        const filterDownload = await  $('#f-Download')
-        await filterDownload.click()
-
-        // Select the .PDF format
-        const filterPDF = await  $('#ft-pdf')
-        await filterPDF.click()
+        await BestsellerPage.selectFilterDownload()
+        await BestsellerPage.selectFilterPDF()
 
         // Check filters are selected
-        const selectedFilterDownload = await  $('//li[@class="selected-filter"][1]/span[@class="f-title"]')
-        await expect(selectedFilterDownload).toHaveText('Download')
-        const selectedFilterPDF = await  $('//li[@class="selected-filter"][2]/span[@class="f-title"]')
-        await expect(selectedFilterPDF).toHaveText('pdf')
-
-        await browser.pause(5000)
+        await expect(BestsellerPage.filterDownloadSelected).toBeDisplayed()
+        await expect(BestsellerPage.filterPDFSelected).toBeDisplayed()
     })
 })
